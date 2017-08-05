5b9db026e074337525c1c8b580d6cd753867f9ac
Alexandru Ghiura <ghalex@gmail.com>
2017-08-05 16:33:31 +0300

    Update version

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import Box from 'Box'
+import Control from 'Control'
+import Field from 'Field'
+import Input from 'Input'
 
 import theme from './theme'
 
 export {
   Box,
+  Control,
+  Field,
+  Input,
   theme
 }
