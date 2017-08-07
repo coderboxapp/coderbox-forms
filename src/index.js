15d62d0dff8b216c7ba016e606e873e7870c9dc1
Alexandru Ghiura <ghalex@gmail.com>
2017-08-07 15:36:05 +0300

    Remove eslint & eslint-standard

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Box from 'Box'
 import Control from 'Control'
 import ControlGroup from 'ControlGroup'
+import Dropdown from 'Dropdown'
 import Field from 'Field'
 import FieldGroup from 'FieldGroup'
 import Input from 'Input'
+import Menu from 'Menu'
 
 import theme from './theme'
 
@@ -11,8 +13,10 @@ export {
   Box,
   Control,
   ControlGroup,
+  Dropdown,
   Field,
   FieldGroup,
   Input,
+  Menu,
   theme
 }
